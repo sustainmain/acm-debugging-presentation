c6705de7860b81777ee87a6ebfe9b77aba2485ee
Josh Marusek <jm553020@ohio.edu>
2024-11-13 15:33:45 -0500

    add program for setting instruction pointer

---
+++ cpp/4-set-instruction-pointer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+void unusedMethod() {
+    cout << "This method is unused. How did you run it?\n";
+}
+
+
+int main (int argc, char **argv) {
+    cout << "Hello, World!\n";
+
+    return EXIT_SUCCESS;
+}
